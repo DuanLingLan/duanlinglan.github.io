05b1f0251095ddfeeac4163801a47bd6026b4d3b
DuanLingLan <horizon_ezio@outlook.com>
2024-10-25 23:07:01 +1100

    Update posts and posts.json

--- update-and-push.bat
+++ update-and-push.bat
@@ -1,14 +1,13 @@
 @echo off
-REM 进入你的项目目录
 cd C:\Users\Horiz\Desktop\web pack\asheweb
 
-REM 运行脚本以生成 posts.json
 node generatePosts.js
 
-REM 提交更改
-git add .
-git commit -m "Update posts.json"
-git push origin main  REM 或者你使用的分支名称
+git add .  
+
+git commit -m "Update posts and posts.json"  
+
+git push origin main  
 
 echo Update and push completed!
 pause
